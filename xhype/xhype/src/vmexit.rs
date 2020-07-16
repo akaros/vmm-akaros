c7bd9d742ab6eca821c51827ddeeda3a11899716
Lencerf <changyuan.lv@Gmail.com>
2020-07-16 17:35:21 -0400

    handle CPUID
    reference: Akaros/kern/arch/x86/trap.c

--- xhype/xhype/src/vmexit.rs
+++ xhype/xhype/src/vmexit.rs
@@ -2,9 +2,11 @@
 
 use crate::consts::msr::*;
 use crate::consts::x86::*;
+use crate::cpuid::do_cpuid;
 use crate::err::Error;
 use crate::hv::vmx::*;
 use crate::hv::X86Reg;
+use crate::hv::{vmx_read_capability, VMXCap};
 use crate::{GuestThread, VCPU};
 #[allow(unused_imports)]
 use log::*;
@@ -202,4 +204,63 @@ pub fn handle_ept_violation(
 ) -> Result<HandleResult, Error> {
     // we need to handle MMIOs. But for now we just resume the vm.
     Ok(HandleResult::Resume)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// VMX_REASON_CPUID
+////////////////////////////////////////////////////////////////////////////////
+
+// the implementation of handle_cpuid() is inspired by handle_vmexit_cpuid()
+// from Akaros/kern/arch/x86/trap.c
+pub fn handle_cpuid(vcpu: &VCPU, gth: &GuestThread) -> Result<HandleResult, Error> {
+    let eax_in = (vcpu.read_reg(X86Reg::RAX)? & 0xffffffff) as u32;
+    let ecx_in = (vcpu.read_reg(X86Reg::RCX)? & 0xffffffff) as u32;
+
+    let (mut eax, mut ebx, mut ecx, mut edx) = do_cpuid(eax_in, ecx_in);
+    match eax_in {
+        0x1 => {
+            // Set the guest thread id into the apic ID field in CPUID.
+            ebx &= 0x0000ffff;
+            ebx |= (gth.vm.cores & 0xff) << 16;
+            ebx |= (gth.id & 0xff) << 24;
+
+            // Set the hypervisor bit to let the guest know it is virtualized
+            ecx |= CPUID_HV;
+
+            // unset monitor capability, vmx capability, and perf capability
+            ecx &= !(CPUID_MONITOR | CPUID_VMX | CPUID_PDCM);
+
+            // unset osxsave if it is not supported or it is not turned on
+            if ecx & CPUID_XSAVE == 0 || vcpu.read_reg(X86Reg::CR4)? & X86_CR4_OSXSAVE == 0 {
+                ecx &= !CPUID_OSXSAVE;
+            } else {
+                ecx |= CPUID_OSXSAVE;
+            }
+        }
+        0x7 => {
+            // Do not advertise TSC_ADJUST
+            ebx &= !CPUID_TSC_ADJUST;
+        }
+        0xa => {
+            eax = 0;
+            ebx = 0;
+            ecx = 0;
+            edx = 0;
+        }
+        0x4000_0000 => {
+            // eax indicates the highest eax in Hypervisor leaf
+            // https://www.kernel.org/doc/html/latest/virt/kvm/cpuid.html
+            eax = 0x4000_0000;
+        }
+        _ => {}
+    }
+    vcpu.write_reg(X86Reg::RAX, eax as u64)?;
+    vcpu.write_reg(X86Reg::RBX, ebx as u64)?;
+    vcpu.write_reg(X86Reg::RCX, ecx as u64)?;
+    vcpu.write_reg(X86Reg::RDX, edx as u64)?;
+    info!(
+        "cpuid, in: eax={:x}, ecx={:x}; out: eax={:x}, ebx={:x}, ecx={:x}, edx={:x}",
+        eax_in, ecx_in, eax, ebx, ecx, edx
+    );
+    Ok(HandleResult::Next)
 }
