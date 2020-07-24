24f674696fc8e5191c4a8b82e42d7a37ad57e90c
Changyuan Lyu <changyuan.lv@Gmail.com>
2020-07-24 18:45:23 -0400

    hide XSAVES if VMX does not support XSAVES
    Signed-off-by: Changyuan Lyu <changyuan.lv@Gmail.com>

--- xhype/xhype/src/vmexit.rs
+++ xhype/xhype/src/vmexit.rs
@@ -477,6 +477,14 @@ pub fn handle_cpuid(vcpu: &VCPU, gth: &GuestThread) -> Result<HandleResult, Erro
             ecx = 0;
             edx = 0;
         }
+        0xd => {
+            if (vmx_read_capability(VMXCap::CPU2)? >> 32) & CPU_BASED2_XSAVES_XRSTORS == 0 {
+                eax = 0;
+                ebx = 0;
+                ecx = 0;
+                edx = 0;
+            }
+        }
         0x4000_0000 => {
             // eax indicates the highest eax in Hypervisor leaf
             // https://www.kernel.org/doc/html/latest/virt/kvm/cpuid.html
